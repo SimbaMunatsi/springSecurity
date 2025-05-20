ddf62d7375348c1985f9f3f66a8795d0cb6215bf
SimbaMunatsi <vsmunatsi@gmail.com>
2025-05-20 14:40:23 +0200

    user repository added

---
+++ src/main/java/com/security/defense/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.security.defense.repository;
+
+import com.security.defense.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
