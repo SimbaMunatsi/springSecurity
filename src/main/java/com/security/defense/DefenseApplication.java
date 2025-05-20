065788b1fff67b71f0f8d5043716509870f51808
SimbaMunatsi <vsmunatsi@gmail.com>
2025-05-20 13:52:38 +0200

    spring initializr dependencies added

---
+++ src/main/java/com/security/defense/DefenseApplication.java
@@ -0,0 +1,13 @@
+package com.security.defense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/defense/DefenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.defense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
