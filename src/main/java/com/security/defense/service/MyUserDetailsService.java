744cf90d1789ef951936aff7a58bf83ca9695269
SimbaMunatsi <vsmunatsi@gmail.com>
2025-05-22 12:45:35 +0200

    start fron here

---
+++ src/main/java/com/security/defense/service/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.security.defense.service;
+
+import com.security.defense.entity.UserPrincipal;
+import com.security.defense.entity.Users;
+import com.security.defense.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Users user = userRepository.findByUsername(username);
+        if(user==null){
+            System.out.println("user not found");
+            throw new UsernameNotFoundException("user not found");
+        }
+        return new UserPrincipal(user);
+    }
+}
