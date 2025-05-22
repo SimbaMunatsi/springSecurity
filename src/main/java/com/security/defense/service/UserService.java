cecdedd22f399fe60bb17d4e2834637f2fbc264b
SimbaMunatsi <vsmunatsi@gmail.com>
2025-05-22 14:27:30 +0200

    password encryption with BCrypt

--- src/main/java/com/security/defense/service/UserService.java
+++ src/main/java/com/security/defense/service/UserService.java
@@ -3,6 +3,7 @@ package com.security.defense.service;
 import com.security.defense.entity.Users;
 import com.security.defense.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,10 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder((10));
+
     public Users register(Users user){
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
